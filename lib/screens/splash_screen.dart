caa96872add6d33354dc7298bafa8f42bc97fc71
Jitendra <apple@apples-MacBook-Pro.local>
2026-03-13 19:41:37 +0530

    All Screens done

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:mct_prayer_book/constants/app_colors.dart';
+import 'package:mct_prayer_book/screens/main_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,7 +12,64 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    _navigateToMain();
+  }
+
+  void _navigateToMain() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const MainScreen()),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: AppColors.primaryColor);
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset("assets/images/transparentlogo.png", width: 120),
+                const SizedBox(height: 30),
+                Text(
+                  "Welcome To",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.w600,
+                    color: AppColors.whiteColor,
+                    letterSpacing: 1.2,
+                  ),
+                ),
+                Text(
+                  "Maitreya Charitable Trust",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.whiteColor,
+                  ),
+                ),
+                const SizedBox(height: 100),
+                SizedBox(
+                  child: SpinKitPouringHourGlassRefined(
+                    color: AppColors.secondaryColor,
+                    size: 100,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
